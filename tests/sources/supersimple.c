528a0df9fa6eb4a67deb1fc020a572d1f654e2ad
Tomas Jasek <tomsik68@gmail.com>
2017-04-25 14:32:56 +0200

    add super simple test with single variable assignment
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

---
+++ tests/sources/supersimple.c
@@ -0,0 +1,8 @@
+
+int main(void) {
+    int a;
+    a = 15;
+
+    test_assert(a == 15);
+    return 0;
+}
---
+++ tests/supersimple.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TESTS_DIR=`dirname $0`
+source "$TESTS_DIR/test-runner.sh"
+
+run_test "sources/supersimple.c"
